e97c39772116c7d39d4b8b1f776abdecd35499e9
Gurukiran <guru.sode@gmail.com>
2020-01-18 17:29:50 +0530

    Add 3x3 tiles

--- index.js
+++ index.js
@@ -6,19 +6,78 @@ function getTiles(customTiles){
     if(window.numberOfTiles){
         customTiles = window.numberOfTiles;
     }
-    for(var i=0; i<customTiles; i++){
-        var template = `<div class="row">
-        <div class="col-sm-4 jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">1</h1>
-              </div>
-        </div>
-    </div>`
-    render(template, document.querySelector('#tile-box'));
+    switch(customTiles){
+        case 3:
+            getThreeTiles(customTiles);
+            break;
+        case 4:
+            getFourTiles(customTiles);
+            break;
+        case 10:
+            getTenTiles(customTiles);
+            break;
+        default:
+            getThreeTiles(customTiles);
+            break;
     }
 }
 
 function selectDifficultyLevel(numberOfTiles){
     window.numberOfTiles = numberOfTiles
     console.log(numberOfTiles)
+}
+
+function getThreeTiles(customTiles){
+        var template = `<div class="row">
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+            <div class="container">
+                <h1 class="display-4">${1}</h1>
+              </div>
+        </div>
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+        <div class="container">
+            <h1 class="display-4">${2}</h1>
+          </div>
+    </div>
+    <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+    <div class="container">
+        <h1 class="display-4">${3}</h1>
+      </div>
+</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+            <div class="container">
+                <h1 class="display-4">${1}</h1>
+              </div>
+        </div>
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+        <div class="container">
+            <h1 class="display-4">${2}</h1>
+          </div>
+    </div>
+    <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+    <div class="container">
+        <h1 class="display-4">${3}</h1>
+      </div>
+</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+            <div class="container">
+                <h1 class="display-4">${1}</h1>
+              </div>
+        </div>
+        <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+        <div class="container">
+            <h1 class="display-4">${2}</h1>
+          </div>
+    </div>
+    <div class="col-sm-4 jumbotron jumbotron-fluid m-2">
+    <div class="container">
+        <h1 class="display-4">${3}</h1>
+      </div>
+</div>
+    </div>`
+    render(template, document.querySelector('#tile-box'));
 }
